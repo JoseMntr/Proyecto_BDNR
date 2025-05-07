d1d449a4360364d17a1b1fe10a3fdd96210be339
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2025-05-07 17:26:37 -0600

    Update connections

--- connections.py
+++ connections.py
@@ -1,30 +1,32 @@
 import sys
 import os
 
+import Cassandra
+import MongoDB
 import Dgraph.dgraph
 
 def menu():
     print("\n--- MENÚ DE CONEXIONES ---")
     print("Seleccione la base de datos a la que desea conectarse:")
-    
     print("1. CassandraDB")
     print("2. MongoDB")
     print("3. DgraphDB")
+    print("0. Terminar programa")
 
 
 def main():
     while True:
         menu()
-        choice = input("Seleccione una opción (1-3): ")
-        if choice == "1":
+        choice = input("Seleccione una opción: ")
+        if choice == 1:
             print("Conectando a CassandraDB...")
             # Aquí puedes agregar la lógica para conectarte a CassandraDB
-        elif choice == "2":
+        elif choice == 2:
             print("Conectando a MongoDB...")
-        elif choice == "3":
+        elif choice == 3:
             print("Conectando a DgraphDB...")
             Dgraph.dgraph.main()  # Aquí llamamos a la función 'main()' dentro de Dgraph.main
-        elif choice == "0":
+        elif choice == 0:
             print("Saliendo del programa...")
             exit(0)
         else:
