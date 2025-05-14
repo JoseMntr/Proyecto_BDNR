bee31604fb09ead0a8660756ff021f5f706a514f
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2025-05-14 07:32:27 -0600

    Update dgraph.py

--- Dgraph/dgraph.py
+++ Dgraph/dgraph.py
@@ -135,6 +135,38 @@ def main():
         else:
             print("Opción no válida.")
 
+def load_data():
+    # Inicializar Client Stub y Dgraph Client
+    client_stub = create_client_stub()
+    client = create_client(client_stub)
+
+    # Crear schema
+    model.set_schema(client)
+    schemaUp = True
+
+    if not schemaUp:
+        schemaUp = model.set_schema(client)
+    
+    # Cargar datos desde archivos CSV
+    users_uids = populate.load_users("Dgraph/data/nodes/users.csv", client)
+    posts_uids = populate.load_posts("Dgraph/data/nodes/posts.csv", client)
+    topics_uids = populate.load_topics("Dgraph/data/nodes/topics.csv", client)
+    comments_uids = populate.load_comments("Dgraph/data/nodes/comments.csv", client)
+
+    # Crear relaciones después de cargar nodos
+    populate.create_generic_edge(client, "Dgraph/data/edges/follows.csv", users_uids, users_uids, "follower_id", "followed_id", "follows")
+    populate.create_generic_edge(client, "Dgraph/data/edges/post.csv", users_uids, posts_uids, "user_id", "post_id", "posts")
+    populate.create_generic_edge(client, "Dgraph/data/edges/likes.csv", users_uids, posts_uids, "user_id", "post_id", "likes")
+    populate.create_generic_edge(client, "Dgraph/data/edges/shares.csv", users_uids, posts_uids, "user_id", "post_id", "shares")
+    populate.create_generic_edge(client, "Dgraph/data/edges/comments.csv", users_uids, comments_uids, "user_id", "comment_id", "comments")
+    populate.create_generic_edge(client, "Dgraph/data/edges/commented_on.csv", comments_uids, posts_uids, "comment_id", "post_id", "commented_on")
+    populate.create_generic_edge(client, "Dgraph/data/edges/interested_in.csv", users_uids, topics_uids, "user_id", "topic_id", "interested_in")
+    populate.create_generic_edge(client, "Dgraph/data/edges/about.csv", posts_uids, topics_uids, "post_id", "topic_id", "about")
+
+    print("Data loaded successfully.")
+    
+    # Cerrar el cliente y el stub
+    close_client_stub(client_stub)
 
 if __name__ == "__main__":
     try:
