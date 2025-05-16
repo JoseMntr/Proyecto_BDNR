9cba933f73d5b0d692cd0a808640ee7324e43c34
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2025-05-16 11:14:09 -0600

    Update dgraph.py

--- Dgraph/dgraph.py
+++ Dgraph/dgraph.py
@@ -46,7 +46,7 @@ def close_client_stub(client_stub):
     client_stub.close()
 
 
-def main():
+def main(option):
     # Inicializar Client Stub y Dgraph Client
     client_stub = create_client_stub()
     client = create_client(client_stub)
@@ -55,85 +55,99 @@ def main():
     model.set_schema(client)
     schemaUp = True
 
-    while True:
-        print_menu()
-        option = int(input("Introduce tu selección: "))
-        if option == 1:
-            if not schemaUp:
-                schemaUp = model.set_schema(client)
-            # Cargar datos desde archivos CSV
-            users_uids = populate.load_users("Dgraph/data/nodes/users.csv", client)
-            posts_uids = populate.load_posts("Dgraph/data/nodes/posts.csv", client)
-            topics_uids = populate.load_topics("Dgraph/data/nodes/topics.csv", client)
-            comments_uids = populate.load_comments("Dgraph/data/nodes/comments.csv", client)
+    # print_menu()
+    # option = int(input("Introduce tu selección: "))
+    if option == 1:
+        if not schemaUp:
+            schemaUp = model.set_schema(client)
+        # Cargar datos desde archivos CSV
+        users_uids = populate.load_users("Dgraph/data/nodes/users.csv", client)
+        posts_uids = populate.load_posts("Dgraph/data/nodes/posts.csv", client)
+        topics_uids = populate.load_topics("Dgraph/data/nodes/topics.csv", client)
+        comments_uids = populate.load_comments("Dgraph/data/nodes/comments.csv", client)
 
-            # # Crear relaciones después de cargar nodos
-            populate.create_generic_edge(client, "Dgraph/data/edges/follows.csv", users_uids, users_uids, "follower_id", "followed_id", "follows")
+        # # Crear relaciones después de cargar nodos
+        populate.create_generic_edge(client, "Dgraph/data/edges/follows.csv", users_uids, users_uids, "follower_id", "followed_id", "follows")
 
-            populate.create_generic_edge(client, "Dgraph/data/edges/post.csv", users_uids, posts_uids, "user_id", "post_id", "posts")
-            populate.create_generic_edge(client, "Dgraph/data/edges/likes.csv", users_uids, posts_uids, "user_id", "post_id", "likes")
-            populate.create_generic_edge(client, "Dgraph/data/edges/shares.csv", users_uids, posts_uids, "user_id", "post_id", "shares")
+        populate.create_generic_edge(client, "Dgraph/data/edges/post.csv", users_uids, posts_uids, "user_id", "post_id", "posts")
+        populate.create_generic_edge(client, "Dgraph/data/edges/likes.csv", users_uids, posts_uids, "user_id", "post_id", "likes")
+        populate.create_generic_edge(client, "Dgraph/data/edges/shares.csv", users_uids, posts_uids, "user_id", "post_id", "shares")
 
-            populate.create_generic_edge(client, "Dgraph/data/edges/comments.csv", users_uids, comments_uids, "user_id", "comment_id", "comments")
-            populate.create_generic_edge(client, "Dgraph/data/edges/commented_on.csv", comments_uids, posts_uids, "comment_id", "post_id", "commented_on")
+        populate.create_generic_edge(client, "Dgraph/data/edges/comments.csv", users_uids, comments_uids, "user_id", "comment_id", "comments")
+        populate.create_generic_edge(client, "Dgraph/data/edges/commented_on.csv", comments_uids, posts_uids, "comment_id", "post_id", "commented_on")
 
-            populate.create_generic_edge(client, "Dgraph/data/edges/interested_in.csv", users_uids, topics_uids, "user_id", "topic_id", "interested_in")
-            populate.create_generic_edge(client, "Dgraph/data/edges/about.csv", posts_uids, topics_uids, "post_id", "topic_id", "about")
+        populate.create_generic_edge(client, "Dgraph/data/edges/interested_in.csv", users_uids, topics_uids, "user_id", "topic_id", "interested_in")
+        populate.create_generic_edge(client, "Dgraph/data/edges/about.csv", posts_uids, topics_uids, "post_id", "topic_id", "about")
 
-            print("Data loaded successfully.")
+        print("Data loaded successfully.")
+        return
 
-        elif option == 2:
-            u1 = input("ID del seguidor: ")
-            u2 = input("ID del seguido: ")
-            model.follow_user(u1, u2, client)
-        elif option == 3:
-            u1 = input("ID del que deja de seguir: ")
-            u2 = input("ID del usuario a dejar de seguir: ")
-            model.unfollow_user(u1, u2, client)
-        elif option == 4:
-            u = input("ID del usuario: ")
-            p = input("ID del post: ")
-            model.like_post(u, p, client)
-        elif option == 5:
-            u = input("ID del usuario: ")
-            p = input("ID del post: ")
-            text = input("Texto del comentario: ")
-            model.comment_post(u, p, text, client)
-        elif option == 6:
-            u = input("ID del usuario: ")
-            model.get_following(u, client)
-        elif option == 7:
-            u = input("ID del usuario: ")
-            model.get_followers(u, client)
-        elif option == 8:
-            p = input("ID del post: ")
-            model.get_comments(p, client)
-        elif option == 9:
-            u1 = input("ID del primer usuario: ")
-            u2 = input("ID del segundo usuario: ")
-            model.get_mutual_followers(u1, u2, client)
-        elif option == 10:
-            u = input("ID del usuario: ")
-            model.recommend_users(u, client)
-        elif option == 11:
-            u = input("ID del usuario: ")
-            model.recomend_users_by_interaction(u, client)
-        elif option == 12:
-            u = input("ID del usuario: ")
-            model.recommend_posts(u, client)
-        elif option == 13:
-            u = input("ID del usuario: ")
-            model.recommend_posts_to_share(u, client)
-        elif option == 14:
-            model.drop_all(client)
-            schemaUp = False
-        elif option == 15:
-            # model.drop_all(client)
-            close_client_stub(client_stub)
-            # exit(0)
-            return
-        else:
-            print("Opción no válida.")
+    elif option == 2:
+        u1 = input("ID del seguidor: ")
+        u2 = input("ID del seguido: ")
+        model.follow_user(u1, u2, client)
+        return
+    elif option == 3:
+        u1 = input("ID del que deja de seguir: ")
+        u2 = input("ID del usuario a dejar de seguir: ")
+        model.unfollow_user(u1, u2, client)
+        return
+    elif option == 4:
+        u = input("ID del usuario: ")
+        p = input("ID del post: ")
+        model.like_post(u, p, client)
+        return
+    elif option == 5:
+        u = input("ID del usuario: ")
+        p = input("ID del post: ")
+        text = input("Texto del comentario: ")
+        model.comment_post(u, p, text, client)
+        return
+    elif option == 6:
+        u = input("ID del usuario: ")
+        model.get_following(u, client)
+        return
+    elif option == 7:
+        u = input("ID del usuario: ")
+        model.get_followers(u, client)
+        return
+    elif option == 8:
+        p = input("ID del post: ")
+        model.get_comments(p, client)
+        return
+    elif option == 9:
+        u1 = input("ID del primer usuario: ")
+        u2 = input("ID del segundo usuario: ")
+        model.get_mutual_followers(u1, u2, client)
+        return
+    elif option == 10:
+        u = input("ID del usuario: ")
+        model.recommend_users(u, client)
+        return
+    elif option == 11:
+        u = input("ID del usuario: ")
+        model.recomend_users_by_interaction(u, client)
+        return
+    elif option == 12:
+        u = input("ID del usuario: ")
+        model.recommend_posts(u, client)
+        return
+    elif option == 13:
+        u = input("ID del usuario: ")
+        model.recommend_posts_to_share(u, client)
+        return
+    elif option == 14:
+        model.drop_all(client)
+        schemaUp = False
+    elif option == 15:
+        # model.drop_all(client)
+        close_client_stub(client_stub)
+        # exit(0)
+        return
+    else:
+        print("Opción no válida.")
+
+
 
 def load_data():
     # Inicializar Client Stub y Dgraph Client
